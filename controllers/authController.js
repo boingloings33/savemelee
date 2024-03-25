7152fda66ee3e6598b93ef5beff268d978f29a58
NEPTR33 <baiolemi@gmail.com>
2024-03-25 14:56:40 -0400

    refactor

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-const Email = require("../utils/email");
 
 const signToken = function (id) {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
